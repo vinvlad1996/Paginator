07b4b4e317e5504130cc9ec5da6b8845f492624f
Владимир <93733168+vinvlad1996@users.noreply.github.com>
2022-02-01 22:50:28 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,57 @@
-fetch('http://testtask.alto.codes/front-products.php')
-  .then(data => data.text())
-  .then(response => {
-    const result = JSON.parse(response);
-    const prod = document.getElementById('product');
+class PaginationPage {
+  static apiBase = 'http://testtask.alto.codes/front-products.php';
 
-    const products = result.map(el => {
-      return `
+  page = 0;
+  perPage = 4;
+
+  constructor() {
+    this.fetchAndRender();
+  }
+
+  fetchAndRender() {
+    fetch(`${PaginationPage.apiBase}?skip=${this.skip}`)
+      .then(data => data.json())
+      .then(response => {
+        const prod = document.getElementById('product');
+
+        prod.innerHTML = response.products.map(el => {
+          return `
+        <div class="container">
         <div class="product">
+          <img src="${el.image_url}" id="image_url" alt="${el.name}">
+          <div id="btn">
+            <button>В наличии</button>
+          </div>
           <div id="name">${el.name}</div>
-          <div id="image_url">${el.image_url}</div>
           <div id="price">${el.price}</div>
         </div>  
+        </div>  
         `;
-    }).join('');
+        }).join('');
 
-    prod.innerHTML = products;
+        return Promise
+      });
+  }
 
-    return Promise
-  })
+  nextPage() {
+    this.page++;
+    this.fetchAndRender();
+  }
+
+  previousPage() {
+    this.page--;
+    this.fetchAndRender();
+  }
+
+  setPage(page) {
+    this.page = page;
+    this.fetchAndRender();
+  }
+
+  get skip() {
+    if (this.page === 0) return 0;
+    return this.page * this.perPage;
+  }
+}
+
+const paginator = new PaginationPage();
